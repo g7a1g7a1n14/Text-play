1fdf15d70b90564b22fa2e8ca4b93b0b3bacca56
g7a1g7a1n14 <100964115+g7a1g7a1n14@users.noreply.github.com>
2022-04-21 10:54:17 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './style.css'
 const Navbar = (props) => {
   return (
     <div>
-      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      {/* <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="/">{props.title}</a>
           <div class={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
@@ -11,7 +11,17 @@ const Navbar = (props) => {
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode === 'light'?'dark':'light'} mode</label>
           </div>
         </div>
-      </nav>
+      </nav> */}
+      <div className="header">
+        <div className='top-heading'><b>{props.title}</b></div>
+      </div>
+      <div className="theme">
+        <div className="dark feature" onClick={() => {props.toggleMode("dark")}}>DARK</div>
+        <div className="light" onClick={() => {props.toggleMode("light")}}>LIGHT</div>
+        <div className="nature feature" onClick={() => {props.toggleMode("nature")}}>NATURE</div>
+        <div className="cool feature" onClick={() => {props.toggleMode("cool")}}>KOOL</div>
+        <div className="space feature" onClick={() => {props.toggleMode("space")}}>CHEER</div>
+      </div>
     </div>
   )
 }
