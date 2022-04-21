febac651c98a008046d4e17556406c8ecf248207
g7a1g7a1n14 <100964115+g7a1g7a1n14@users.noreply.github.com>
2022-04-21 10:53:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Form from './Form';
 import {useState} from 'react'
 const App = () => {
   const [mode,setMode] = useState("light");
-  const toggleMode = () =>{
-    if(mode === 'light'){ 
-      setMode('dark');
-      document.body.style.backgroundColor = '#131516';
-    }
-    else{
-      setMode('light')
+  const toggleMode = (cls) =>{
+    if(cls === 'light'){ 
+      setMode('white');
       document.body.style.backgroundColor = 'white';
+    }
+    if(cls === 'dark'){ 
+      setMode('black');
+      document.body.style.backgroundColor = 'black';
+    }
+    if(cls === 'nature'){ 
+      setMode('green');
+      document.body.style.backgroundColor = 'green';
+    }
+    if(cls === 'cool'){ 
+      setMode('blue');
+      document.body.style.backgroundColor = 'blue';
+    }
+    if(cls === 'space'){ 
+      setMode('purple');
+      document.body.style.background = 'purple';
     }
   }
   return (
     <>
-      <Navbar title ="TEXT ANALYZER " mode = {mode} toggleMode = {toggleMode}/>
+      <Navbar title ="TEXT PLAY " mode = {mode} toggleMode = {toggleMode}/>
       <Form heading = "Enter Your Text To Analyze" mode = {mode}/>
       {/* <About /> */}
     </>
